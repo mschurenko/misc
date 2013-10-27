1f57943498578584ff79616c2596dbc62fd56fe1
Matt Schurenko <mschurenko@contractor.ea.com>
2013-10-26 19:25:40 -0700

    added sb-install.sh

---
+++ sb-install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#set -x
+
+# search, install, update, redo
+
+origin='git://slackbuilds.org/slackbuilds.git'
+repo='/home/matt/slackbuilds'
+usage="\nusage: $0 [search|install|info|update|redo]\n"
+
+chk_repo() {
+	if [[ -d $repo ]];then
+		cd $repo
+	else
+		echo "$repo doesn't exist"
+		exit 4
+	fi
+}
+
+dl_sb() {
+	chk_repo
+	if ! [[ -d $1 ]];then
+		echo "$1 doesn't exist"
+		exit 3
+	fi	
+	tmp=`mktemp -d`
+	rsync -a $1/ $tmp
+	cd $tmp
+}
+
+com=$1
+pat=$2
+
+case $com in
+	search)
+		chk_repo
+		pkg=(`find . -type d -name "*$2*"` )
+		if [[ -z $pkg ]];then
+			echo "$2 didin't match."
+			exit 2
+		fi
+		for p in "${pkg[@]}";do
+			echo $p
+		done
+	;;
+	install)
+		dl_sb $pat
+		url=`awk -F= '/DOWNLOAD=/ {print $NF}' *.info | sed 's/"//g'`
+		wget $url
+		chmod +x ./*.SlackBuild
+		output=`mktemp`
+		sudo ./*.SlackBuild | tee $output
+		sbpkg=`perl -lne 'print $1 if /^Slackware package (.*\.tgz)/' $output`
+		sudo installpkg $sbpkg
+		rm -rf $tmp
+	;;
+	info)
+		dl_sb $pat
+		cat README
+		echo
+		cat *.info
+		rm -rf $tmp
+	;;
+	update)
+		chk_repo
+		git pull
+	;;
+	redo)
+		cd
+		rm -rf $repo
+		git clone $origin
+	;;
+	*)
+		echo -e $usage
+esac
