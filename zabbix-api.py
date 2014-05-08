6db8db9b2873036457fb7c803ce062bd0e0fb12e
Matt Schurenko <matts@image-engine.com>
2014-05-08 12:37:23 -0700

    adding zabbix-api.py

--- zabbix-api.py
+++ zabbix-api.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# bulk add render nodes to Renderfarm zabbix group and link them to Template_Render_Blade template
+# batch_file should be a list of render node host names (eg: rd06001) one on each line
+
 import json
 import requests
 import sys
@@ -8,76 +11,89 @@ import getpass
 
 url = 'http://ie-monitor/zabbix/api_jsonrpc.php'
 header = { 'content-type' : 'application/json' }
-#hn = subprocess.Popen('hostname',stdout=subprocess.PIPE).stdout.read().split('.')[0]
 group_id = '11'
 template_id = '10077'
-hn = 'rd06001'
 domain = '.ie.dom'
 ip = '10.65.7.1'
 port = '10050'
+batch_file = ''
 
-sys.stdout.write('enter username: ')
-user = sys.stdin.readline().strip()
+try:
+  batch_file = sys.argv[1]
+except:
+  print 'usage: %s <batch file>' % sys.argv[0]
 
-pw = getpass.getpass()
+try:
+  hosts = open(batch_file, 'r')
+except:
+  print '%s not found' % batch_file
+  sys.exit(2)
 
-auth_pl = { 'jsonrpc' : '2.0', 'method' : 'user.login', 'params' : { 'user' : user, 'password' : pw }, 'id' : 1 }
+# functions
+def zabbix_auth():
+  sys.stdout.write('enter zabbix username: ')
+  user = sys.stdin.readline().strip()
+  pw = getpass.getpass('enter zabbix password: ')
+  
+  auth_pl = { 'jsonrpc' : '2.0', 'method' : 'user.login', 'params' : { 'user' : user, 'password' : pw }, 'id' : 1 }
+  r = requests.post(url, data=json.dumps(auth_pl), headers=header)
+  ret = r.json()
+  if 'error' in ret:
+    return 'login must have failed'
+  else:
+    return ret['result']
 
-r = requests.post(url, data=json.dumps(auth_pl), headers=header)
+def host_add(hn,ip,tok):
+  hostadd_pl = {
+    'jsonrpc' : '2.0',
+    'method' : 'host.create',
+    'params' : {
+      'host' : hn,
+      'interfaces' : [
+        {
+          'type' : 1,
+          'main' : 1,
+          'useip' : 0,
+          'dns' : hn + domain,
+          'ip' : ip,
+          'port' : port
+        }
+      ],
+      'groups' : [
+        {
+          'groupid' : group_id
+        }
+      ],
+      'templates' : [
+        {
+          'templateid' : template_id
+        }
+      ]
+    },
+    'auth' : tok,
+    'id' : 1
+  }
 
-ret = r.json()
-if 'error' in ret:
-  print 'login must have failed'
-  print ret['error']
-  sys.exit(1)
+  # check if host exists
+  hosts_pl = { 'jsonrpc' : '2.0', 'method' : 'host.exists', 'params' : { 'host' : hn }, 'auth' : tok, 'id' : 1 }
+  r = requests.post(url, data=json.dumps(hosts_pl), headers=header)
+  res = r.json()['result']
 
-tok = r.json()['result']
+  if res == False:
+    r = requests.post(url, data=json.dumps(hostadd_pl), headers=header)
+    return r.text
+    #return '%s does not exists' % hn  
+  else:
+    return '%s already exists' % hn  
 
-hosts_pl = { 'jsonrpc' : '2.0', 'method' : 'host.exists', 'params' : { 'host' : hn }, 'auth' : tok, 'id' : 1 }
-r = requests.post(url, data=json.dumps(hosts_pl), headers=header)
+# main
+tok = zabbix_auth()
+if tok.startswith('login'):
+  print tok
+  sys.exit(1) 
 
-#print r.status_code
-#print r.text
-
-res = r.json()['result']
-hostadd_pl = {
-  'jsonrpc' : '2.0',
-  'method' : 'host.create',
-  'params' : {
-    'host' : hn,
-    'interfaces' : [
-      {
-        'type' : 1,
-        'main' : 1,
-        'useip' : 0,
-        'dns' : hn + domain,
-        'ip' : ip,
-        'port' : port
-      }
-    ],
-    'groups' : [
-      {
-        'groupid' : group_id
-      }
-    ],
-    'templates' : [
-      {
-        'templateid' : template_id
-      }
-    ]
-  },
-  'auth' : tok,
-  'id' : 1
-}
-
-#r = requests.post(url, data=json.dumps(hostadd_pl), headers=header)
-#print r.text
-
-if res == False:
-  #print type(res)
-  print 'result is false'
-  # add host to group
-  r = requests.post(url, data=json.dumps(hostadd_pl), headers=header)
-  print r.text
-else:
-  print "host %s already exists!" % hn
+for host in hosts: 
+  host = host.strip()
+  ip = subprocess.Popen(['host',host],stdout=subprocess.PIPE).stdout.read().split()[-1]
+  ret = host_add(host,ip,tok)
+  print ret
